11b678533d1c5c0e4e431dcc96735c0e7bedb4bc
aunnnn <aun.wirawit@gmail.com>
2017-03-03 14:41:51 +0700

    Test commit 2

--- pages/texts.js
+++ pages/texts.js
@@ -4,7 +4,7 @@ import Page from '../components/Page';
 export default () => {
   return (
     <Page title="Texts">
-      <div>It will be written. Soon.</div>
+      <div>It will be written. Soon. Test</div>
     </Page>
   );
 };
