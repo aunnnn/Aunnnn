473ba20be3999f027131c328c2807a04994d82c2
aunnnn <aun.wirawit@gmail.com>
2017-03-13 02:34:43 +0700

    Add simple login and admin page

---
+++ pages/admin.js
@@ -0,0 +1,239 @@
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import shortid from 'shortid';
+import moment from 'moment';
+import _ from 'lodash';
+import Page from '../components/Page';
+
+function checkStatus(res) {
+  if (res.error) {
+    const err = res.error;
+    const error = new Error(err.message);
+    error.response = res;
+    throw error;
+  }
+  return res;
+}
+
+class admin extends Component {
+
+  static async getInitialProps() {
+    const res = await fetch('http://localhost:3001/api/Posts');
+    const json = await res.json();
+    return { posts: json };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: props.posts,
+      loading: false,
+      findingToken: true,
+      newPostTitle: '',
+      newPostContent: '',
+      newPostSlug: '',
+    };
+
+    // data
+    this.checkToken = this.checkToken.bind(this);
+    this.refreshPosts = this.refreshPosts.bind(this);
+
+    // rendering
+    this.NonEmptyTable = this.NonEmptyTable.bind(this);
+    this.renderValidatedContent = this.renderValidatedContent.bind(this);
+
+    // form handling
+    this.handleAddNewPost = this.handleAddNewPost.bind(this);
+    this.handleNewPostFormChange = this.handleNewPostFormChange.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.checkToken(), 80);
+  }
+
+  // go to home page if no permission
+  checkToken() {
+    const token = localStorage.getItem('aunnnn-token');
+    if (!token) {
+      this.setState({ findingToken: false });
+      this.props.url.push('/');
+      return;
+    }
+    this.setState({ token, findingToken: false });
+  }
+
+  refreshPosts() {
+    fetch('http://localhost:3001/api/Posts')
+    .then(res => res.json())
+    .then(checkStatus)
+    .then(posts => this.setState({ posts }))
+    .catch(alert);
+  }
+
+  PostTableRow(post) {
+    return (
+      <tr key={shortid.generate()}>
+        <td>{post.title}</td>
+        <td>{post.content.length}</td>
+        <td>{post.slug}</td>
+        <td>{moment(post.created_at).fromNow()}</td>
+        <td><a>edit</a></td>
+      </tr>
+    );
+  }
+
+  NonEmptyTable(posts) {
+    return (
+      <table className="u-full-width">
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>length</th>
+            <th>slug</th>
+            <th>create at</th>
+            <th>manage</th>
+          </tr>
+        </thead>
+        <tbody>
+          { _.sortBy(posts, 'created_at').reverse().map(this.PostTableRow) }
+        </tbody>
+      </table>
+    );
+  }
+
+  handleAddNewPost(event) {
+    event.preventDefault();
+
+    const { loading, token } = this.state;
+    if (loading) { return; }
+    if (!token) {
+      alert('Oops! No token.');
+      return;
+    }
+
+    this.setState({ loading: true });
+
+    const { newPostTitle, newPostContent, newPostSlug } = this.state;
+    fetch(`http://localhost:3001/api/Posts?access_token=${token}`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        created_at: Date(),
+        title: newPostTitle,
+        content: newPostContent,
+        slug: newPostSlug,
+      }),
+    })
+    .then(res => res.json())
+    .then(checkStatus)
+    .then(res => {
+      alert('The new post is added!');
+      this.setState({
+        newPostTitle: '',
+        newPostContent: '',
+        newPostSlug: '',
+      });
+      this.refreshPosts();
+    })
+    .catch(alert)
+    .then(() => {
+      this.setState({ loading: false });
+    });
+  }
+
+  handleNewPostFormChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  renderValidatedContent() {
+    const { posts, newPostTitle, newPostContent, newPostSlug } = this.state;
+
+    return (
+      <div style={{ padding: '20px' }}>
+        <h4><strong>Texts</strong></h4>
+
+        {/* NEW POST FORM*/}
+        <form
+          onSubmit={this.handleAddNewPost}
+          style={{
+            padding: '20px',
+            borderStyle: 'solid',
+            borderWidth: '1px',
+            borderColor: 'grey',
+          }}
+        >
+          <h4>New Post</h4>
+          <div className="row">
+            <label className="two columns" htmlFor="newPostTitle">Title</label>
+            <input
+              className="ten columns"
+              type="string"
+              id="newPostTitle"
+              value={this.state.newPostTitle}
+              onChange={this.handleNewPostFormChange}
+            />
+          </div>
+
+          <div className="row">
+            <label className="two columns" htmlFor="newPostContent">Content</label>
+            <textarea
+              className="ten columns"
+              id="newPostContent"
+              value={this.state.newPostContent}
+              onChange={this.handleNewPostFormChange}
+            />
+          </div>
+
+          <div className="row">
+            <label className="two columns" htmlFor="newPostSlug">Slug</label>
+            <input
+              className="ten columns"
+              type="string"
+              id="newPostSlug"
+              value={this.state.newPostSlug}
+              onChange={this.handleNewPostFormChange}
+            />
+          </div>
+
+          <input
+            value="Save"
+            type="submit"
+            disabled={newPostTitle === '' || newPostContent === '' || newPostSlug === ''}
+            className="button-primary"
+          />
+        </form>
+
+        <div>
+          <div>
+            { posts.length === 0 ?
+              (
+                <div style={{ textAlign: 'center', verticalAlign: 'middle' }}>
+                  No posts yet.
+                </div>
+              )
+              :
+              this.NonEmptyTable(posts) }
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { token, findingToken } = this.state;
+    return (
+      <Page title="Admin">
+        {
+          (findingToken && <h5>Loading...</h5>)
+          || (!findingToken && !token && <h5>Unauthorized.</h5>)
+          || this.renderValidatedContent()
+        }
+      </Page>
+    );
+  }
+}
+
+export default admin;
---
+++ pages/login.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import Page from '../components/Page';
+
+function checkStatus(res) {
+  if (res.error) {
+    const err = res.error;
+    const error = new Error(err.message);
+    error.response = res;
+    throw error;
+  }
+  return res;
+}
+
+class admin extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      username: '',
+      password: '',
+      loading: false,
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+
+    if (this.state.loading) { return; };
+
+    this.setState({
+      loading: true,
+    });
+
+    const { username, password } = this.state;
+
+    fetch('http://localhost:3001/api/Users/login', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+    .then(res => res.json())
+    .then(checkStatus)
+    .then(res => {
+      localStorage.setItem('aunnnn-token', res.id);
+      this.props.url.push('/admin');
+    })
+    .catch(err => {
+      alert(err);
+      this.setState({
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Page title="Manage">
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <label htmlFor="usernameInput">username</label>
+            <input
+              className="u-full-width"
+              type="text"
+              onChange={this.handleChange}
+              value={this.state.username}
+              id="username"
+            />
+          </div>
+          <div className="row">
+            <label htmlFor="passwordInput">password</label>
+            <input
+              className="u-full-width"
+              type="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+              id="password"
+            />
+          </div>
+          <div className="row">
+            {
+              !this.state.loading && (
+                <input
+                  className="button-primary"
+                  type="submit"
+                  value="Login"
+                  disabled={!this.state.username || !this.state.password}
+                />
+              )
+            }
+          </div>
+        </form>
+      </Page>
+    );
+  }
+}
+
+export default admin;
