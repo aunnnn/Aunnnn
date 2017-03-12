612ee3b850ae74134e94fcf5d57343baca37c289
aunnnn <aun.wirawit@gmail.com>
2017-03-12 11:56:14 +0700

    Add progress bar.

--- components/DefaultHead.js
+++ components/DefaultHead.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.onRouteChangeStart = (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 export default ({ title = 'Aunnnn' }) => (
   <Head>
@@ -7,5 +16,6 @@ export default ({ title = 'Aunnnn' }) => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel="stylesheet" type="text/css" href="/static/css/normalize.css" />
     <link rel="stylesheet" type="text/css" href="/static/css/skeleton.css" />
+    <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css" />
   </Head>
 );
